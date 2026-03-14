a16dd7ec3f9a2da4b74d51e526a304764cb1224b
Karthikeya Jammula <jammula.karthikeya8@gmail.com>
2026-03-14 22:21:02 +0530

    Time: 3 ms (25.15%), Space: 46.4 MB (45.72%) - LeetHub

---
+++ 3866-first-unique-even-element/3866-first-unique-even-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstUniqueEven(int[] nums) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i:nums){
+            hm.put(i,hm.getOrDefault(i,0)+1);
+        }
+        for(int i : nums){
+            if(i%2==0 && hm.get(i)==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
