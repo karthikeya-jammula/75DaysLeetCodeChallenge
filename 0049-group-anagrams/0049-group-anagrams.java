e7a1c213d0e2f241cd6ff50f74dbcde4ef2f09a6
Karthikeya Jammula <jammula.karthikeya8@gmail.com>
2026-03-16 21:45:28 +0530

    Time: 7 ms (75.92%), Space: 49.7 MB (58.25%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,39 +1,16 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> ans = new ArrayList<>();
-        List<String> li = new ArrayList<>();
-        li.add(strs[0]);
-        ans.add(li);
-        int n = strs.length;
-        for(int i=1;i<n;i++) {
-            boolean f = false;
-            for(List<String> a : ans) {
-                if(check(a.get(0),strs[i])){
-                    a.add(strs[i]);
-                    f=true;
-                    break;
-                }
+        HashMap<String,List<String>> hm = new HashMap<>();
+        for(String s : strs) {
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String k = new String(ch);
+            if(!hm.containsKey(k)){
+                // List<String> li = new ArrayList<>();
+                hm.put(k,new ArrayList<>());
             }
-            if(!f){
-                    List<String> cur = new ArrayList<>();
-                    cur.add(strs[i]);
-                    ans.add(cur);
-            }
+            hm.get(k).add(s);
         }
-        return ans;
-    }
-    public static boolean check(String n,String m) {
-        if(n.length()!=m.length()) return false;
-        char[] a = n.toCharArray();
-        char[] b = m.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        int x = a.length;
-        for(int i=0;i<x;i++) {
-            if(a[i]!=b[i]){
-                return false;
-            }
-        }
-        return true;
+        return new ArrayList<>(hm.values());
     }
 }
