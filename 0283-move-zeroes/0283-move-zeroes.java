fda4b8aecfc0f85e74878bd26f09831b67c56279
Karthikeya Jammula <jammula.karthikeya8@gmail.com>
2026-03-21 15:47:50 +0530

    Time: 130 ms (5.19%), Space: 47.6 MB (85.17%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,7 +1,22 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n = nums.length;
-        if(n==1) return nums;
-        int i=0,
+        int n=nums.length;
+        int k=0;
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+            nums[k]=nums[i];
+            k++;
+            }
+            else
+            c++;
+        }
+        for(int i=0;i<c;i++){
+            nums[k]=0;
+            k++;
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(nums[i]);
+        }
     }
 }
