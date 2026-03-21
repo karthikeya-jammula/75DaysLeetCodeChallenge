446225889088198415269096d307c47d3cdabdfb
Karthikeya Jammula <jammula.karthikeya8@gmail.com>
2026-03-21 15:47:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,22 +1,7 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n=nums.length;
-        int k=0;
-        int c=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0){
-            nums[k]=nums[i];
-            k++;
-            }
-            else
-            c++;
-        }
-        for(int i=0;i<c;i++){
-            nums[k]=0;
-            k++;
-        }
-        for(int i=0;i<n;i++){
-            System.out.print(nums[i]);
-        }
+        int n = nums.length;
+        if(n==1) return nums;
+        int i=0,
     }
 }
